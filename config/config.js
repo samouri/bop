f82def53b40fe022f06f9b3b449f865b85e3f2d0
Jake Fried <jzfisgreat@gmail.com>
2016-05-28 19:49:20 -0400

    configs sorta setup

---
+++ config/config.js
@@ -0,0 +1,36 @@
+var config = {};
+config.mongodb = {};
+config.http = {};
+
+config.http.cookie_secret = process.env.HTTP_COOKIE_SECRET || 'YeukhPqijei86QWt3TBwhfjNe';
+config.http.enforce_ssl = process.env.HTTP_ENFORCE_SSL;
+config.http.host_url = process.env.HTTP_HOST_URL || 'http://localhost';
+
+config.pathToTestMongoDb = 'mongodb://localhost/test';
+config.mongodb.host = process.env.MONGODB_HOST || '127.0.0.1';
+config.mongodb.port = process.env.MONGODB_PORT || 27017;
+config.mongodb.user = process.env.MONGODB_USERNAME || '';
+config.mongodb.password = process.env.MONGODB_PASSWORD || '';
+config.mongodb.database = process.env.MONGODB_DATABASE || 'test';
+
+
+// Prod vs. Dev config
+if (process.env.NODE_ENV === "production") {
+  config.pathToMongoDb = 'mongodb://localhost/prod';
+  config.indexPath = "index.prod.html";
+  config.host = "nothingtoseehere.xyz";
+  config.port = "3000";
+  config.prod = true;
+  config.dev = false;
+}
+else {
+  config.pathToMongoDb = 'mongodb://ssh.nothingtoseehere.xyz/test';
+  config.indexPath = "index.html";
+  config.host = "192.168.0.2:3000";
+  config.port = "3001";
+  config.assetsPort = "8090";
+  config.prod = false;
+  config.dev = true;
+}
+
+module.exports = config;
