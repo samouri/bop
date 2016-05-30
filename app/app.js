d89f35b68f2dc470c34eea7cfd97ee8b7c7fe913
derek petersen <derekpetersen28@gmail.com>
2016-05-29 23:37:04 -0700

    removing jakes bad codes

--- app/app.js
+++ app/app.js
@@ -2,14 +2,13 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, browserHistory } from 'react-router'
 
-var Landing = require('./components/landing.js');
+//var Landing = require('./components/landing.js');
 var About = require('./components/about.js');
 
 
 var router = (
     <Router history={browserHistory}>
-      <Route path="/" component={Landing}/>
-      <Route path=":region" handler={Landing}/>
+      <Route path="/" component={About}/>
     </Router>
 );
 
