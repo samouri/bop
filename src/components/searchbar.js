06e8a04893ff46e2b533c72247aff0c6a1a5dfae
Jake F - @samouri <samouri@users.noreply.github.com>
2017-05-05 01:42:08 -0400

    bye bye bloodhound

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,77 +1,56 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+import _ from 'lodash';
 
 import { mapSpotifyItemToBop } from '../sdk';
 
+const spotifyEndpoint = 'https://api.spotify.com/v1/search?type=track&limit=5';
+
+const loadOptions = input => {
+	return fetch(`${spotifyEndpoint}&q=${input}`)
+		.then(response => {
+			return response.json();
+		})
+		.then(json => {
+			const ret = { options: mapResponseToTitles(json) };
+			return ret;
+		});
+};
+
+const mapResponseToTitles = resp => {
+	const tracks = resp.tracks.items.map(mapSpotifyItemToBop);
+	// tracks.forEach(track => (this.data[track.title] = track));
+	// console.error(tracks.map(track => track.title));
+	return tracks.map(track => ({ value: track, label: track.title }));
+	// return tracks;
+};
+
+class TrackValue extends React.Component {
+	render() {
+		const { children, placeholder, option } = this.props;
+		const { thumbnail_url, artist, title } = option.value;
+		console.error(option);
+		return (
+			<div className="search-result-thumbnail-wrapper">
+				<img src={thumbnail_url} />
+				{title} by {artist}
+			</div>
+		);
+	}
+}
+
 /* eslint-disable */
-// TODO find a replacement for bloodhound + typeahead seeing as they are basically deprecated projects
-// ideally one that is react-friendly
 export default class SearchBar extends React.Component {
-	state = {
-		userInput: '',
-	};
-
-	handleChange = event => {
-		this.setState({ userInput: event.target.value });
-	};
-
-	componentDidMount() {
-		this.setupTypeahead();
-	}
-
-	setupTypeahead() {
-		var _this = this;
-		_this.data = {};
-		_this.youtubeVideos = new Bloodhound({
-			// eslint-disable-line
-			datumTokenizer: Bloodhound.tokenizers.obj.whitespace, // eslint-disable-line
-			queryTokenizer: Bloodhound.tokenizers.whitespace, // eslint-disable-line
-			remote: {
-				url: 'https://api.spotify.com/v1/search?type=track&limit=5&q=%QUERY',
-				wildcard: '%QUERY',
-				transform: function(resp) {
-					const tracks = resp.tracks.items.map(mapSpotifyItemToBop);
-					tracks.forEach(track => (_this.data[track.title] = track));
-					return tracks.map(track => track.title);
-				},
-			},
-		});
-
-		$(ReactDOM.findDOMNode(_this.refs.typeahead)).typeahead(null, {
-			// eslint-disable-line
-			source: _this.youtubeVideos,
-			templates: {
-				suggestion: function(val) {
-					console.error(_this.data, _this.data[val]);
-					return (
-						'<div class="search-result-thumbnail-wrapper">' +
-						`<img src="${_this.data[val].thumbnail_url}"></img>` +
-						`${val} by ${_this.data[val].artist}` +
-						'</div>'
-					);
-				},
-			},
-		});
-
-		$(ReactDOM.findDOMNode(_this.refs.typeahead)).on('typeahead:selected', function(evt, selected) {
-			_this.props.handleSelection(_this.data[selected]);
-		});
-	}
-
 	render() {
 		return (
-			<div>
-				<input
-					id="typeahead"
-					type="text"
-					ref="typeahead"
-					className="pull-right"
-					placeholder="Add song"
-					value={this.state.userInput}
-					onChange={this.handleChange}
-				/>
-				<i className="fa fa-search fa-2x pull-right"> </i>
-			</div>
+			<Select.Async
+				name="form-field-name"
+				value="one"
+				loadOptions={loadOptions}
+				autoload={false}
+				optionComponent={TrackValue}
+			/>
 		);
 	}
 }
