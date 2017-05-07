ec890bdb90a9902541e54c0ad3b9d531d34bcec5
Jake F - @samouri <samouri@users.noreply.github.com>
2017-05-07 18:28:58 -0400

    fix swaggerHost because postgres doesnt know its own hostname

--- src/config.js
+++ src/config.js
@@ -3,24 +3,16 @@ var config = {
 };
 
 const prod = process.env.NODE_ENV === 'production';
-console.log(process.env.NODE_ENV);
 
 if (prod) {
 	// prod means in docker
 	config.swaggerUrl = '/swagger.yaml';
+	config.swaggerHost = window.location.hostname;
 } else {
 	config.swaggerUrl = 'http://' + window.location.hostname + ':3333';
 	config.swaggerHost = window.location.hostname + ':3333';
-	console.error(config.swaggerUrl);
 }
 
-// // either docker instance and localhost (hooking up local dockers ) OR
-// // specified with url params (npm start)
-// if (
-// 	(prod && window.location.hostname === 'localhost') ||
-// 	new URLSearchParams(window.location.search).has('swaggerHost')
-// ) {
-// 	config.swaggerHost = 'localhost:3333';
-// }
+console.log(process.env.NODE_ENV, 'config: ', config);
 
 export default config;
