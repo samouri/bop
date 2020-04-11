d5d13942b03f5d227cb85c916e6e154115cbdb50
Jake F <samouri@users.noreply.github.com>
2020-04-10 23:51:23 -0400

    upgrade react-scripts and use it instead of react-scripts-ts

--- src/components/top-contributors.tsx
+++ src/components/top-contributors.tsx
@@ -1,16 +1,16 @@
-import * as React from "react";
-import { connect } from "react-redux";
-import * as _ from "lodash";
-import { Link } from "react-router-dom";
+import * as React from 'react'
+import { connect } from 'react-redux'
+import * as _ from 'lodash'
+import { Link } from 'react-router-dom'
 
-import { getContributorsInPlaylist } from "../state/reducer";
+import { getContributorsInPlaylist } from '../state/reducer'
 
 type StateProps = {
-  contributors: string[];
-};
-type OwnProps = { playlistId: number | undefined };
+  contributors: string[]
+}
+type OwnProps = { playlistId: number | undefined }
 
-type Props = StateProps & OwnProps;
+type Props = StateProps & OwnProps
 
 class TopContributors extends React.Component<Props> {
   render() {
@@ -18,18 +18,16 @@ class TopContributors extends React.Component<Props> {
       <span className="topcontributors__contrib" key={name}>
         <Link to={`/u/${name}`}>@{name}</Link>
       </span>
-    ));
+    ))
     return (
       <div className="topcontributors">
         <span> Top Contributors</span>
         <span className="topcontributors__list">{contribs}</span>
       </div>
-    );
+    )
   }
 }
 
-export default connect<StateProps, any, OwnProps>(
-  (state, ownProps: OwnProps) => ({
-    contributors: getContributorsInPlaylist(state, ownProps.playlistId),
-  })
-)(TopContributors);
+export default connect<StateProps, any, OwnProps>((state, ownProps: OwnProps) => ({
+  contributors: getContributorsInPlaylist(state, ownProps.playlistId) as any,
+}))(TopContributors)
