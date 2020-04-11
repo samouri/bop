5f3c03fe01b0a5eae16da2b6bf0ac98a23ef0652
Jake F <samouri@users.noreply.github.com>
2020-04-11 01:57:18 -0400

    whoops, forgot some files

---
+++ src/generated/apis/index.ts
@@ -0,0 +1,7 @@
+export * from './EventsApi';
+export * from './IntrospectionApi';
+export * from './MetadataApi';
+export * from './PlaylistsApi';
+export * from './SongsApi';
+export * from './UsersApi';
+export * from './VotesApi';
